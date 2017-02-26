1d766baead610a748ae8a534370001e420dabc36
Xavier JUERY <xavier@juery.fr>
2017-02-26 22:59:01 +0100

    Added Theme install

--- install.sh
+++ install.sh
@@ -100,6 +100,11 @@ echo "" >> ~/.bashrc
 echo "source ~/dotfiles/my_bashrc" >> ~/.bashrc
 echo "" >> ~/.bashrc
 
+# INSTALL THEME ONLY FOR UBUNTU 16.04
+sh -c "echo 'deb http://download.opensuse.org/repositories/home:/Horst3180/xUbuntu_16.04/ /' > /etc/apt/sources.list.d/arc-theme.list"
+curl http://download.opensuse.org/repositories/home:Horst3180/xUbuntu_16.04/Release.key | apt-key add -
+apt update && apt install arc-theme
+
 # FINISH & CLEAN
 apt autoremove
 apt autoclean
