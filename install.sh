57a9663afe677cf941beb992481047af5723cf4e
Xavier JUERY <xavier@juery.fr>
2017-03-08 21:04:57 +0100

    Added some new apt cmd & added chown for some dir

--- install.sh
+++ install.sh
@@ -1,3 +1,8 @@
+#####################################################################
+# Post-install Initialisation script
+# (Script compatible with *Ubuntu 16.04.x)
+#####################################################################
+
 # UTILITY FUNCTIONS
 cleanFile(){
 	local name=$1
@@ -51,6 +56,7 @@ export NVM_DIR="$HOME/.nvm" && (
   cd "$NVM_DIR"
   git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" origin`
 ) && . "$NVM_DIR/nvm.sh"
+chown -R "$USER" "$NVM_DIR"
 
 nvm install node
 
@@ -66,6 +72,12 @@ apt install -y google-chrome-stable
 # INSTALL JAVA 8
 apt install -y oracle-java8-installer
 
+# INSTALL VLC
+apt install VLC
+
+# INSTALL Calibre
+apt install calibre
+
 # INSTALL keeweb
 wget -O keeweb.deb https://github.com/keeweb/keeweb/releases/download/v1.4.0/KeeWeb-1.4.0.linux.x64.deb
 dpkg -i keeweb.deb
@@ -75,6 +87,7 @@ cleanFile keeweb.deb
 # INSTALL WEBSTORM
 mkdir -p $HOME/bin
 curl -sSL "https://download.jetbrains.com/webstorm/WebStorm-2016.3.3.tar.gz" | tar -v -C $HOME/bin -xz
+chown -R "$USER" $HOME/bin
 
 # INSTALL GOLANG
 export GO_VERSION=1.8
@@ -100,7 +113,7 @@ echo "" >> ~/.bashrc
 echo "source ~/dotfiles/my_bashrc" >> ~/.bashrc
 echo "" >> ~/.bashrc
 ln -s ~/dotfiles/face.jpg ~/.face
-chown xavier ~/.face
+chown "$USER" ~/.face
 
 # INSTALL THEME ONLY FOR UBUNTU 16.04
 sh -c "echo 'deb http://download.opensuse.org/repositories/home:/Horst3180/xUbuntu_16.04/ /' > /etc/apt/sources.list.d/arc-theme.list"
